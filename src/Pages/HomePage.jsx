23f05b316d2d64f0f44be800575a95fa28063091
MicheleTodeschini <micheletodeschini01@gmail.com>
2025-12-11 15:45:28 +0100

    add map

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,14 +1,46 @@
 import Header from "../../components/Header"
 import Footer from "../../components/Footer"
+import { useEffect, useState } from "react"
+import axios from "axios"
 
 
 export default function Homepgae() {
 
+    const [films, setFilms] = useState([])
 
+    const url = 'http://localhost:3000/api/movies/'
+
+    useEffect(getFilms, [])
+
+    function getFilms() {
+        axios
+            .get(url)
+            .then(res => setFilms(res.data))
+    }
 
     return (
         <>
             <Header />
+            <div className="container">
+                <div className="row">
+                    {
+                        films.map(film => (
+                            <div key={film.id} className="col-4 card">
+                                <div className="card-top">
+                                    <img src={`http://localhost:3000/img/${film.image}`}></img>
+                                </div>
+                                <div className="card-bottom">
+                                    <h3>{film.title}</h3>
+                                    <p>{film.abstract}</p>
+                                    <p>{film.director}, {film.genre}, {film.release_year}</p>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+
+
             <Footer />
         </>
     )
