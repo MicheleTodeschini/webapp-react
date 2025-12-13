fae824470f00d7cd9f117206c9c2e47084690feb
MicheleTodeschini <micheletodeschini01@gmail.com>
2025-12-13 18:33:06 +0100

    add card to single film page. REsized of the img, add scale up on hover

--- src/Pages/FilmInfoPage.jsx
+++ src/Pages/FilmInfoPage.jsx
@@ -3,6 +3,7 @@ import Footer from "../../components/Footer"
 import axios from "axios"
 import { Link, useParams } from "react-router-dom"
 import { useEffect, useState } from "react"
+import Homepgae from "./HomePage"
 
 
 export default function FilmInfoPage() {
@@ -21,20 +22,24 @@ export default function FilmInfoPage() {
     return (
         <>
             <Header />
+            <Link to={'/'}>Return to the HomePage</Link>
+
+
             <h3>Vedo il film con id {film.id}</h3>
 
-            <div key={film.id} className="col-4">
-                <div className="card-top">
+            <div key={film.id} className="info">
+                <div className="card-left">
                     <img src={`http://localhost:3000/img/${film.image}`}></img>
                 </div>
-                <div className="card-bottom">
+                <div className="card-right">
                     <h3>{film.title}</h3>
                     <p>{film.abstract}</p>
                     <p>{film.director}, {film.genre}, {film.release_year}</p>
-                    <Link to={`/FilmInfoPage/${film.id}`}>Click here for seeing more about the film</Link>
+
                 </div>
 
             </div>
+
 
             <Footer />
         </>
